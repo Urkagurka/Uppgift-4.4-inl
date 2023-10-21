7ba623faa64965fdad40f809123de90a2d60c444
Urkagurka <143172181+Urkagurka@users.noreply.github.com>
2023-10-21 16:09:17 +0200

    Add project files.

---
+++ Uppgift 4.4 inl/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Uppgift_4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välj ett av följande alternativ");
+            Console.WriteLine("1. Subtrahera ett tal med ett annat");
+            Console.WriteLine("2. Dividera ett tal med ett annat");
+            Console.WriteLine("3. Avsluta");
+            int VAL = int.Parse(Console.ReadLine());
+
+            if( VAL == 1 )
+            {
+                Console.WriteLine("skriv in tal 1");
+                int tal1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("skriv in tal 2");
+                int tal2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Det blir " + (tal1 - tal2));
+                Console.ReadKey();
+            }
+            else if (VAL == 2)
+            {
+                Console.WriteLine("skriv in tal 1");
+                int tal1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("skriv in tal 2");
+                int tal2 = int.Parse(Console.ReadLine());
+                if (tal2 == 0)
+                {
+                    Console.WriteLine("Det blir oändligt");
+                }
+                else
+                {
+                    Console.WriteLine("Det blir " + (tal1 / tal2));
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
